6e0d387dfc195ef96c17e73e0d2337dd7bf8561b
PurabB <balanipb@gmail.com>
2024-04-27 17:00:19 -0700

    added map

--- src/components/map/GoogleMaps.tsx
+++ src/components/map/GoogleMaps.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
+import { Loader } from '@googlemaps/js-api-loader'
 
 const GoogleMaps = () => {
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const initializeMap = async() => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_API_KEY as string,
+        version: 'quarterly',   
+      });
+
+      const {Map} = await loader.importLibrary('maps');
+
+      const locationInMap = {
+        lat: 39.60,
+        lng: -9.07,
+      };
+      // MARKER
+      const{Marker} = (await loader.importLibrary('marker')) as google.maps.MarkerLibrary;
+
+      const options: google.maps.MapOptions = {
+        center: locationInMap,
+        zoom: 15,
+        mapId: 'NEXT_MAP',
+      };
+      const map = new Map(mapRef.current as HTMLDivElement, options)
+
+      //add marker
+      const marker = new Marker({
+        map: map,
+        position: locationInMap,
+      })
+    };
+    initializeMap();
+  }, []);
+
     return (
-      <div>GoogleMaps</div>
+      <div className="h-[950px]" ref = {mapRef}>Google Maps</div>
     );
   };
   
