ddf0daedd3b4bceaa9d3e0ccbd56accbb3bdeb51
kellyma073 <kma073@ucr.edu>
2024-04-27 17:08:07 -0700

    worked on about

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import React from "react";
 import About from "@/components/home/About"
-import FindHospitals from "@/components/home/FindHospitals";
 
 const page = () => {
   return (
     <div>
         <About />
-        <FindHospitals />
     </div>
   );
 };
--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 const About = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center justify-center h-screen text-6xl text-white font-serif font-bold">
-        BearCare
-      </div>
-
-      <div className="flex items-center justify-center v-screen text-white font-serif text-center">
-        Say goodbye to expensive medical bills with BearCare. <br/> Easily find nearby hospitals for your insurance plan today!
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div>
+          <div className="text-6xl text-white font-serif font-bold text-center">BearCare</div>
+          <div className="flex items-center justify-center text-white font-serif text-center pt-5 pb-5">
+            Say goodbye to expensive medical bills with BearCare. <br/> Easily find nearby hospitals for your insurance plan today!
+          </div>
+            <div className="text-center" >Find Hospitals</div>
+            <IoIosArrowDown className="flex justify-center items-center ml-48" size="25"/>
+        </div>
       </div>
     </div>
   );
