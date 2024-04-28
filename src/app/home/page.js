65fdcd4b0bab1fe4b2b79a83a2228e33dd645b8f
Vaneesha Singh <vsingh@ucr-secure-43-10-13-221-177.wnet.ucr.edu>
2024-04-27 21:46:47 -0700

    fix

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import About from "@/components/home/About";
-import FindHospitals from "@/components/home/FindHospitals";
 import Provider from "@/components/home/Provider";
 import texts from "@/data/Providers";
 
@@ -8,7 +7,6 @@ const page = () => {
   return (
     <div>
         <About />
-        <FindHospitals />
         <div className="flex flex-col justify-center items-center">
           <div className="grid grid-rows-4 grid-cols-3 gap-x-10 w-6/12">
           {texts.map((paragraph, index) => (
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import React from "react";
 import About from "@/components/home/About";
-import FindHospitals from "@/components/home/FindHospitals";
 import Provider from "@/components/home/Provider";
 
 const page = () => {
   return (
     <div>
         <About />
-        <FindHospitals />
         <Provider />
     </div>
   );
